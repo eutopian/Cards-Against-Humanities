867ce9202b2f6f3a86767f9cb1718b32b821c972
Margaret Ma <eutopia@gmail.com>
2018-04-13 11:17:17 -0400

    webpack config wip

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: 'webpack-bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, "./"),
+    watchContentBase: true
+  }
+};
